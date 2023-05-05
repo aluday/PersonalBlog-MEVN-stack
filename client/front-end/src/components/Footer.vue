964ed26d99bf24d57c4932d18835eb271aca07e1
aluday <anhluu322@gmail.com>
2023-05-05 09:33:28 +0700

    add link

--- client/front-end/src/components/Footer.vue
+++ client/front-end/src/components/Footer.vue
@@ -64,7 +64,7 @@
         <!-- Copyright -->
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
             © 2020 Copyright:
-            <a class="text-white" href="#">Lưu Thị Dương Anh</a>
+            <a class="text-white" href="https://github.com/aluday/PersonalBlog-MEVN-stack">Lưu Thị Dương Anh</a>
         </div>
         <!-- Copyright -->
     </footer>
